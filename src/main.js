afc6beb1a259fa9301f63d38eaf27bbdbe4e19a6
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-11-23 18:18:52 -0500

    Wrote code

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
 const axios = require("../utils/axios");
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "http://localhost:5001";
 
-function getAllNames() {}
+function getAllNames() {
+  axios
+    .get(`${BASE_URL}/constellations`)
+    .then(response => {
+      const data = response.data;
+      const names = [];
+      for (let stars of data) {
+        names.push(stars.name);
+      }
+      console.log(names;)
+    })
+}
 
-function getConstellationsByQuadrant(quadrant) {}
+function getConstellationsByQuadrant(quadrant) {
+  axios
+    .get(`${BASE_URL}/constellations`)
+    .then(response => {
+      const starConstellations = response.data.filter(stars => stars.quadrant === quadrant);
+      console.log(starConstellations);
+    })
+}
 
 module.exports = {
   getAllNames,
